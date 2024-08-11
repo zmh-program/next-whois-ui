2bc4698496dc7fb03c58e9218e405c6b083e5cfb
Zhang Minghan <zmh13054618081@dingtalk.com>
2024-08-11 23:54:28 +0800

    chore: update tapline

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -48,7 +48,7 @@ const DrawerContent = React.forwardRef<
       )}
       {...props}
     >
-      <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-muted" />
+      <div className="mx-auto mt-2 md:mt-4 h-1 w-10 md:h-2 md:w-[100px] rounded-full bg-muted" />
       {children}
     </DrawerPrimitive.Content>
   </DrawerPortal>

0085f76f7279655dbf64b707fd1f8d3970aa9de3
Zhang Minghan <zmh13054618081@dingtalk.com>
2024-08-11 21:34:11 +0800

    chore: update events

--- src/components/PWAInstaller.tsx
+++ src/components/PWAInstaller.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import "@khmyznikov/pwa-install";
+import {useEffect} from "react";
 export default function PWAInstaller({ ...props }) {
   return (
     // @ts-ignore
@@ -10,6 +11,7 @@ export default function PWAInstaller({ ...props }) {
 
 export type PWAInstallerMethods = {
   install: (force?: boolean) => void;
+  addEventListener: (type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions) => void;
 };
 
 export function usePWAInstaller() {
@@ -18,10 +20,43 @@ export function usePWAInstaller() {
       "pwa-install",
     )[0] as unknown as PWAInstallerMethods;
 
+  useEffect(() => {
+    const installer = getInstallerElement();
+    if (installer) {
+      // register events
+
+      installer.addEventListener("pwa-install-success-event", (e) => {
+        console.log("[installer] installation success:", e);
+      });
+
+      installer.addEventListener("pwa-install-fail-event", (e) => {
+        console.error("[installer] installation failed:", e);
+      });
+
+      installer.addEventListener("pwa-install-available-event", (e) => {
+        console.log("[installer] installation available:", e);
+      });
+
+      installer.addEventListener("pwa-user-choice-result-event", (e) => {
+        console.log("[installer] user choice result:", e);
+      });
+
+      installer.addEventListener("pwa-install-how-to-event", (e) => {
+        console.log("[installer] installation how to:", e);
+      });
+
+      installer.addEventListener("pwa-install-gallery-event", (e) => {
+        console.log("[installer] installation gallery:", e);
+      });
+    }
+  }, []);
+
   return {
     install: (force?: boolean) => {
       const installer = getInstallerElement();
       installer?.install(force);
+
+      console.log(`[installer] ${force ? "forced" : "prompted"} installation to:`, installer);
     },
   };
 }

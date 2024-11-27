300459b49386b15a9a897221c02e280121250032
Minghan Zhang <112773885+zmh-program@users.noreply.github.com>
2024-11-27 15:02:04 +0800

    Update lookup.ts

--- src/pages/api/lookup.ts
+++ src/pages/api/lookup.ts
@@ -1,8 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { lookupWhoisWithCache } from "@/lib/whois/lookup";
 import { WhoisAnalyzeResult } from "@/lib/whois/types";
-import { i18n } from "next-i18next";
-
 type Data = {
   status: boolean;
   time: number;
@@ -20,13 +18,13 @@ export default async function handler(
   if (!query || typeof query !== "string" || query.length === 0) {
     return res
       .status(400)
-      .json({ time: -1, status: false, error: i18n?.t("Query is required") });
+      .json({ time: -1, status: false, error: "Query is required" });
   }
 
   const { time, status, result, error, cached } =
     await lookupWhoisWithCache(query);
   if (!status) {
-    return res.status(500).json({ time, status, error: i18n?.t(error) });
+    return res.status(500).json({ time, status, error });
   }
 
   return res.status(200).json({ time, status, result, cached });
